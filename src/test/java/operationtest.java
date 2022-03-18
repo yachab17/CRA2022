f3cf6c66dc87837dddf7a4d7ef3c841c2d460570
jungrok heo <hjl6374@naver.com>
2022-03-18 15:35:21 +0900

    [test] Junt Test 폴더 생성 및 동작 확인 코드 추가

---
+++ src/test/java/operationtest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class operationtest {
+
+    @Test
+    void junit_test(){
+        assertEquals(1, test.return_test());
+    }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,5 @@
+public class test {
+    public static int return_test(){
+        return 1;
+    }
+}
