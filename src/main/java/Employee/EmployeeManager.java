17d810d66e7ee145bf14da2615e1a68e17a50206
KJH <bssmkjh@gmail.com>
2022-03-21 08:45:37 +0900

    [Feature] Employee Manager CRUD 스켈레톤 코드 추가
    [Test] Employee Manager CRUD 테스트 코드 추가
    [Etc] Git Ignore 파일 삭제

---
+++ src/main/java/Employee/EmployeeManager.java
@@ -0,0 +1,48 @@
+package Employee;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EmployeeManager {
+    private static EmployeeManager employeeManager = new EmployeeManager();
+    private Map<Integer, Employee> employeeMap;
+
+    private EmployeeManager() {
+        employeeMap = new HashMap<Integer, Employee>();
+    }
+
+    public static EmployeeManager GetInstance() {
+        return employeeManager;
+    }
+
+    public void addCommand(Employee employee) {
+    }
+
+    public List<Employee> delteCommand(Command command, ISearch searcher) {
+        List<Employee> resultEmployees = new ArrayList<Employee>();
+
+        return resultEmployees;
+    }
+
+    public List<Employee> updateCommand (Command command, ISearch searcher) {
+        List<Employee> resultEmployees = new ArrayList<Employee>();
+
+        return resultEmployees;
+    }
+
+    public List<Employee> searchCommand(Command command, ISearch searcher) {
+        List<Employee> resultEmployees = new ArrayList<Employee>();
+
+        return resultEmployees;
+    }
+
+    public Map<Integer, Employee> getEmployees() {
+        return employeeMap;
+    }
+
+    public int getTotalEmployees() {
+        return employeeMap.size();
+    }
+}
