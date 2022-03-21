aa4c44550eea9cd9198b68114ad03d215e6359fe
KJH <bssmkjh@gmail.com>
2022-03-21 16:20:41 +0900

    [Feature] Employee Manager excute 함수 추가, search factory 함수 구현
    [Test] excute add 테스트 추가

---
+++ src/main/java/Employee/EmployeeNumberSearch.java
@@ -0,0 +1,13 @@
+package Employee;
+
+import Command.Command;
+
+import java.util.List;
+import java.util.Map;
+
+public class EmployeeNumberSearch implements ISearch {
+    @Override
+    public List<Integer> search(Map<Integer, Employee> employeemap, Command command) {
+        return null;
+    }
+}
