9ec85e2f84ecbba236e5d018a3f41ed1e522f8aa
jungrok heo <hjl6374@naver.com>
2022-03-21 22:48:43 +0900

    [Feature] Search(without option) 구체 클래스 구현

--- src/main/java/Employee/BirthDaySearch.java
+++ src/main/java/Employee/BirthDaySearch.java
@@ -2,12 +2,22 @@ package Employee;
 
 import Command.Command;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BirthDaySearch implements ISearch {
     @Override
     public List<Integer> search(Map<Integer, Employee> employeemap, Command command) {
-        return null;
+        List<Integer> resultList = new ArrayList<>();
+
+        Iterator<Map.Entry<Integer, Employee>> entries = employeemap.entrySet().iterator();
+
+        while (entries.hasNext()) {
+            Map.Entry<Integer, Employee> entry = entries.next();
+            if (entry.getValue().getBirthDay().equals(command.getSourceValue())) {
+                resultList.add(entry.getKey());
+            }
+        }
+
+        return resultList;
     }
 }
--- src/main/java/Employee/NameSearch.java
+++ src/main/java/Employee/NameSearch.java
@@ -2,12 +2,29 @@ package Employee;
 
 import Command.Command;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class NameSearch implements ISearch {
     @Override
     public List<Integer> search(Map<Integer, Employee> employeemap, Command command) {
-        return null;
+
+        List<Integer> resultList = new ArrayList<>();
+
+        Iterator<Map.Entry<Integer, Employee>> entries = employeemap.entrySet().iterator();
+
+        if((command.getOption2() != null) || (command.getOption3() !=null)){
+            // optional search
+        }
+        else{
+            while(entries.hasNext()){
+                Map.Entry<Integer, Employee> entry = entries.next();
+                if(entry.getValue().getName().equals(command.getSourceValue())){
+                    resultList.add(entry.getKey());
+
+                }
+            }
+        }
+
+        return resultList;
     }
 }
